d68b2ecddce37a61300e9f5adef47c26207e3437
Megamaruthu5 <megavarshini400@gmail.com>
2026-02-04 17:38:05 +0530

    Added Job role skilled dataset

---
+++ job_roles_data.py
@@ -0,0 +1,27 @@
+job_roles = {
+    "Data Scientist": [
+        "Python",
+        "Machine Learning",
+        "Statistics",
+        "Data Visualization",
+        "Pandas",
+        "NumPy"
+    ],
+
+    "AI Engineer": [
+        "Python",
+        "Deep Learning",
+        "TensorFlow",
+        "PyTorch",
+        "NLP",
+        "Computer Vision"
+    ],
+
+    "Web Developer": [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React",
+        "Backend Development"
+    ]
+}
